739e4d3dca120adcb919293393ee3626b507c86f
BrlessKoin <jlescalonap@gmail.com>
2016-07-09 13:56:00 -0400

    Add files via upload

--- Vumork.java
+++ Vumork.java
@@ -1,12 +1,15 @@
 package vumork;
 
+import javax.swing.JOptionPane; 
+
 public class Vumork {
 
     public static void main(String[] args) {
         lvl usrlvl= new lvl();
-        usrlvl.usrpts = 35;
+        usrlvl.usrnm =("Dragsu");
+        usrlvl.usrpts =78;
         
-    usrlvl.lvlcalc(0);
+    usrlvl.lvlcalc(usrlvl.usrpts);
         
     }
     
