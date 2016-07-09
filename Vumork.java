74fad5cf18e47a2ad1f69c0397f79f98014936b1
Dragsu <angelbriceno@gmail.com>
2016-07-09 12:54:37 -0400

    Add files via upload

--- Vumork.java
+++ Vumork.java
@@ -4,10 +4,9 @@ public class Vumork {
 
     public static void main(String[] args) {
         lvl usrlvl= new lvl();
-        usrlvl.usrnm = ("migas");
         usrlvl.usrpts = 35;
         
-    usrlvl.lvlcalc();
+    usrlvl.lvlcalc(0);
         
     }
     
